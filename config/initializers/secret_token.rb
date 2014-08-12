79c3ca08ccf4a98900f0db63e4c706fea865b11b
Mike Culver <mculver@ptc.com>
2014-08-12 15:27:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntegrityAdapter::Application.config.secret_key_base = '9c34071f2b3a4334ac7103b63995732309cf2385dbaac60248232ed027e3c0369e2e0ed7f3628c6dd0d090019fc650551c7596e1de6a61728ae69482b3c9559a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IntegrityAdapter::Application.config.session_store :cookie_store, key: '_IntegrityAdapter_session'
